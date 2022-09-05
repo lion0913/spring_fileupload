c924a083e5be1ed1d08355df0ac3391eb2e79335
박다정 <ekwjd990913@gmail.com>
2022-09-05 14:59:07 +0900

    나머지 환경 설정(baseEntity, db setting, 파일 생성용 기본 엔티티 등) 완료

---
+++ src/main/java/com/ll/exam/spring_fileupload/base/BaseConfig.java
@@ -0,0 +1,16 @@
+package com.ll.exam.spring_fileupload.base;
+
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+
+@Configuration
+public class BaseConfig {
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
+        return new JPAQueryFactory(entityManager);
+    }
+}
