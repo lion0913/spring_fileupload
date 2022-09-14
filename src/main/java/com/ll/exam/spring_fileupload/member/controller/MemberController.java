adacdb022a2bede2f212ac78255cc99d21c6b675
박다정 <ekwjd990913@gmail.com>
2022-09-15 01:36:00 +0900

    프로필 이미지 url 줄이기

--- src/main/java/com/ll/exam/spring_fileupload/member/controller/MemberController.java
+++ src/main/java/com/ll/exam/spring_fileupload/member/controller/MemberController.java
@@ -10,10 +10,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.ServletException;
@@ -88,4 +85,8 @@ public class MemberController {
         return memberContext;
     }
 
+    @GetMapping("/profile/img/{id}")
+    public String showProfileImg(@PathVariable Long id) {
+        return "redirect:" + memberService.findById(id).getProfileImgPath();
+    }
 }
