8ec4d3a7bfb2cb8533103d56ff1b4876856f5296
박다정 <ekwjd990913@gmail.com>
2022-09-19 14:17:00 +0900

    디비 안붙어지는 버그 수정

--- src/main/java/com/ll/exam/spring_fileupload/SpringFileuploadApplication.java
+++ src/main/java/com/ll/exam/spring_fileupload/SpringFileuploadApplication.java
@@ -3,6 +3,7 @@ package com.ll.exam.spring_fileupload;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootApplication
 @EnableJpaAuditing
